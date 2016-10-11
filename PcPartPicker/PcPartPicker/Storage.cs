840369fa1e1c166c6a62c0f06e9b671049173089
danny bergh <dandeman111@gmail.com>
2016-10-11 11:09:49 +0200

    ik heb de storage class gemaakt

---
+++ PcPartPicker/PcPartPicker/Storage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcPartPicker
+{
+    class Storage
+    {
+        //properties
+        public int Storage_id { get; set; }
+        public int Speed { get; set; }
+        public string Interface { get; set; }
+        public int Price { get; set; }
+
+        //constructor
+        public Storage(int storage_id,int speed,string interFace,int price)
+        {
+            this.Storage_id = storage_id;
+            this.Speed = speed;
+            this.Interface = interFace;
+            this.Price = price;
+        }
+    }
+}
