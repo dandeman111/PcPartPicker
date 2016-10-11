9084afe55b1608b5d9d92781a24a74f1b543420b
danny bergh <dandeman111@gmail.com>
2016-10-11 11:30:47 +0200

    Computer class is gemaakt

---
+++ PcPartPicker/PcPartPicker/Computer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcPartPicker
+{
+    class Computer
+    {
+        //properties
+        public int Computer_id { get; set; }
+        public Cpu Cpu { get; set; }
+        public Motherboard Motherboard { get; set; }
+        public Memory Memory { get; set; }
+        public Storage Storage { get; set; }
+        public List<Gpu> Gpus { get; set; }
+        public List<User> Owner { get; set; }
+        public int Price { get; set; }
+
+        //constructor
+        public Computer(int computer_id)
+        {
+            this.Computer_id = computer_id;
+        }
+
+    }
+}
--- PcPartPicker/PcPartPicker/User.cs
+++ PcPartPicker/PcPartPicker/User.cs
@@ -12,7 +12,7 @@ namespace PcPartPicker
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-       // public List<computer>{ get;set;}
+        public List<Computer> Computers{ get;set;}
 
         //constructor
         public User(string username,string password,string email)
