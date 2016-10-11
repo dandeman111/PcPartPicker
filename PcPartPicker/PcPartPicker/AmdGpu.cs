36f643bf7eaec3c74271cd3b817663df3e31216f
danny bergh <dandeman111@gmail.com>
2016-10-11 11:20:49 +0200

    AmdGpu Class is gemaakt

---
+++ PcPartPicker/PcPartPicker/AmdGpu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcPartPicker
+{
+    class AmdGpu : Gpu
+    {
+        //properties
+        public int Crossfire { get; set; }
+
+        //constructor
+        public AmdGpu(int gpu_id, int clockspeed, int vram, int price, int crossfire) : base(gpu_id, clockspeed, vram, price)
+        {
+            this.Crossfire = crossfire;
+        }
+    }
+}
