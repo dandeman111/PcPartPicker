9fbbe8542b7b6b197256e0846c19f7220e20a192
danny bergh <dandeman111@gmail.com>
2016-10-11 11:05:40 +0200

    ik heb de memory class gemaakt

---
+++ PcPartPicker/PcPartPicker/Memory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcPartPicker
+{
+    class Memory
+    {
+        //properties
+        public int Memory_id { get; set; }
+        public int Clockspeed { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+
+        //constructor
+        public Memory(int memory_id,int clockspeed, string type,int price)
+        {
+            this.Memory_id = memory_id;
+            this.Clockspeed = clockspeed;
+            this.Type = type;
+            this.Price = price;
+        }
+    }
+}
