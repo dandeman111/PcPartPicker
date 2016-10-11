a1c0f389cd44f179648be75aaef79e93d1294401
danny bergh <dandeman111@gmail.com>
2016-10-11 11:15:15 +0200

    Gpu class gemaakt

---
+++ PcPartPicker/PcPartPicker/Gpu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcPartPicker
+{
+    abstract class Gpu
+    {
+        //properties
+        protected int Gpu_id { get; set; }
+        protected int Clockspeed { get; set; }
+        protected int Vram { get; set; }
+        protected int Price { get; set; }
+
+        //constructor
+        protected Gpu(int gpu_id,int clockspeed,int vram,int price)
+        {
+            this.Gpu_id = gpu_id;
+            this.Clockspeed = clockspeed;
+            this.Vram = vram;
+            this.Price = price;
+        }
+    }
+}
