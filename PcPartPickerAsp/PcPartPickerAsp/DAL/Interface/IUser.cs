4242618627eca12650d1bffed3945cb1e337d7f3
danny bergh <dandeman111@gmail.com>
2016-12-13 09:38:47 +0100

    User toegevoegd

---
+++ PcPartPickerAsp/PcPartPickerAsp/DAL/Interface/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PcPartPicker;
+
+namespace PcPartPickerAsp.DAL.Interface
+{
+    interface IUser
+    {
+        void Add(User user);
+        User GetById(int id);
+        List<User> GetAll();
+        void Update(User user);
+        void Delete(User user);
+    }
+}
