f7361bc17ab087ec84782170bb65ee655139c950
danny bergh <dandeman111@gmail.com>
2016-12-13 09:37:34 +0100

    I storage en Nvidea gpu togeveogd

---
+++ PcPartPickerAsp/PcPartPickerAsp/DAL/Interface/INvideaGpu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PcPartPicker;
+
+namespace PcPartPickerAsp.DAL.Interface
+{
+    interface INvideaGpu
+    {
+        void Add(NvideaGpu nvideaGpu);
+        NvideaGpu GetById(int id);
+        List<NvideaGpu> GetAll();
+        void Update(NvideaGpu nvideaGpu);
+        void Delete(NvideaGpu nvideaGpu);
+    }
+}
---
+++ PcPartPickerAsp/PcPartPickerAsp/DAL/Interface/IStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PcPartPicker;
+
+namespace PcPartPickerAsp.DAL.Interface
+{
+    interface IStorage
+    {
+        void Add(Storage storage);
+        Storage GetById(int id);
+        List<Storage> GetAll();
+        void Update(Storage storage);
+        void Delete(Storage storage);
+    }
+}
