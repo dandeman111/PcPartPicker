3b50774c11aab65f276181636593fb2590ca8fb5
danny bergh <dandeman111@gmail.com>
2016-12-28 13:25:26 +0100

    computer constructor beetje gefixt

--- PcPartPickerAsp/PcPartPickerAsp/DAL/Models/Computer.cs
+++ PcPartPickerAsp/PcPartPickerAsp/DAL/Models/Computer.cs
@@ -16,13 +16,30 @@ namespace PcPartPickerAsp.DAL.Models
         public List<User> Owner { get; set; }
         public int Price { get; set; }
 
-        private CpuRepo cpuRepo= new CpuRepo();
-
+        private CpuRepo cpuRepo= new CpuRepo(new CpuMssql());
+        private MotherboardRepo motherboardRepo = new MotherboardRepo(new MotherboardMssql());
+        private MemoryRepo memoryRepo = new MemoryRepo(new MemoryMssql());
+        private StorageRepo storageRepo = new StorageRepo(new StorageMssql());
+        private UserRepo user = new UserRepo(new UserMssql());
+#warning nog geen gpu's gefixt
         //constructor
         public Computer(int computerId, int cpuId, int motherboardId, int memoryId, int storageId,List<Gpu> gpus, List<User> owners  )
         {
             ComputerId = computerId;
-            Cpu = 
+            Cpu = cpuRepo.GetById(cpuId);
+            Motherboard = motherboardRepo.GetById(cpuId);
+            Memory = memoryRepo.GetById(memoryId);
+            Storage = storageRepo.GetById(storageId);
+
+            List<User> users = new List<User>();
+            foreach (var x in owners)
+            {
+                users.Add(x);
+            }
+            Owner = users;
+
+
+
         }
 
     }
